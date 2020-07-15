2cb8e9b64b68648d9a2a301ae98c771196bc74cb
nqngoc <ginamvis15@gmail.com>
2020-07-15 12:00:02 +0700

    done interface

--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -4,14 +4,12 @@ import { withStyles } from "@material-ui/core/styles";
 import styles from "./style";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
-import Icon from "@material-ui/core/Icon";
 import Grid from "@material-ui/core/Grid";
 import { STATUSES } from "../../constants";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+
+import TaskList from "./../../components/TaskList";
+
+import TaskForm from "../../components/TaskForm";
 // generate  data
 const listTask = [
   {
@@ -35,12 +33,14 @@ const listTask = [
 ];
 
 class TaskBoard extends Component {
+  state = {
+    open: false,
+  };
   renderBoard() {
-    const { classes } = this.props;
-    console.log(this.props);
+    // console.log(this.props);
     let xhtml = null;
     xhtml = (
-      <Grid container spacing={2}>
+      <Grid container spacing={3}>
         {STATUSES.map((status, index) => {
           //  lấy các task mà task.status (listtask) =  status.value(STATUSES)
           // để đưa vào taskFiltered
@@ -48,59 +48,48 @@ class TaskBoard extends Component {
             (task) => task.status === status.value
           );
 
-          return (
-            <Grid item md={4} sm={6} xs={12} key={status.value}>
-              <Box mt={3} mb={2}>
-                <div className={classes.status}>{status.label}</div>
-              </Box>
-
-              <div className={classes.wrapperListTask}>
-                {taskFiltered.map((task) => {
-                  const { title, description } = task;
-                  return (
-                    <Card key={task.id}>
-                      <CardContent>
-                        <Grid container justify="space-between">
-                          <Grid item md={8}>
-                            <Typography component="h2">{title}</Typography>
-                            <Typography component="h2" variant="h5">
-                              {description}
-                            </Typography>
-                          </Grid>
-                          <Grid item md={4}>
-                            {status.label}
-                          </Grid>
-                        </Grid>
-                      </CardContent>
-                      <CardActions justify="">
-                        <Button size="small">
-                          Edit <Icon fontSize="small">edit</Icon>
-                        </Button>
-                        <Button size="small">
-                          Delete <Icon fontSize="small">delete</Icon>
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  );
-                })}
-              </div>
-            </Grid>
-          );
+          return <TaskList tasks={taskFiltered} status={status} key={index} />;
         })}
       </Grid>
     );
     return xhtml;
   }
 
+  //dialog
+
+  openForm = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  renderForm() {
+    const { open } = this.state;
+    let xhtml = null;
+    xhtml = <TaskForm open={open} onClose={this.handleClose} />;
+    return xhtml;
+  }
+
   render() {
     const { classes } = this.props;
-    // console.log(this.props);
+
     return (
       <div className={classes.taksboard}>
-        <Button variant="contained" color="primary" className={classes.button}>
+        <Button
+          variant="contained"
+          onClick={() => this.openForm()}
+          color="primary"
+          className={classes.button}
+        >
           <AddIcon /> Thêm mới công việc
         </Button>
         {this.renderBoard()}
+        {this.renderForm()}
       </div>
     );
   }
